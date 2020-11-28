f4ef3c27354d517c8a751b518402a390fc00c9f7
Dowling, Eoin (00431819) <Eoin.Dowling@bms.com>
2020-11-28 17:37:18 +0000

    Continued work on User Interface and allowing input to run model from Menu

--- linearModel.py
+++ linearModel.py
@@ -15,33 +15,65 @@ non_zero = power_data[(power_data["speed"]>0) & (power_data["power"]>0)]
 # Combine Data
 clean_data = clean_data.append(non_zero)
 
-# Non-Cleaned Linear Model Class
+# Linear Model Class
 #-------------------------------------------------------------------------------#
-class LinearModel:
-    def train_nc_linear_model():
-        nc_x = power_data["speed"].to_numpy()
-        nc_y = power_data["power"].to_numpy()
 
-        nc_x = nc_x.reshape(-1, 1)
+# c = Cleansed, nc = Non-Cleansed
+class LinearModel:
+    # Trains Non-Cleansed Model
+    def train_nc_linear_model():
+        x = power_data["speed"].to_numpy()
+        y = power_data["power"].to_numpy()
+
+        x = x.reshape(-1, 1)
 
         model = lin.LinearRegression()
-        model.fit(nc_x, nc_y)
+        model.fit(x, y)
 
         return [model.intercept_, model.coef_[0]]
 
-    def nc_linear_model(nc_x, nc_p):
-        return nc_p[0] + nc_x * nc_p[1]
+    def nc_linear_model(x, p):
+        return p[0] + x * p[1]
 
-    def nc_linear_predict(nc_x):
-        return LinearModel.nc_linear_model(nc_x, LinearModel.train_nc_linear_model())
+    def nc_linear_predict(x):
+        return LinearModel.nc_linear_model(x, LinearModel.train_nc_linear_model())
 
+    # Takes input for Non-Cleansed Model
     def nc_linear_input(x):
-        wind_speed = x#float(input("Please Enter Wind Speed: "))
+        wind_speed = x
 
         prediction = LinearModel.nc_linear_predict(wind_speed)
 
         return {"value": prediction}
 
-#LinearModel.nc_linear_input()
+#-------------------------------------------------------------------------------#
+
+    # Trains Cleansed Model
+    def train_c_linear_model():
+        x = clean_data["speed"].to_numpy()
+        y = clean_data["power"].to_numpy()
+
+        x = x.reshape(-1, 1)
+
+        model = lin.LinearRegression()
+        model.fit(x, y)
+
+        return [model.intercept_, model.coef_[0]]
+
+    def c_linear_model(x, p):
+        return p[0] + x * p[1]
+
+    def c_linear_predict(x):
+        return LinearModel.c_linear_model(x, LinearModel.train_c_linear_model())
+
+    # Takes input for Non-Cleansed Model
+    def c_linear_input(x):
+        wind_speed = x
+
+        prediction = LinearModel.c_linear_predict(wind_speed)
+
+        return {"value": prediction}
+
+
 
 #-------------------------------------------------------------------------------#
--- server.py
+++ server.py
@@ -8,11 +8,24 @@ app = fl.Flask(__name__, static_url_path = '', static_folder = 'staticpages')
 def home():
     return app.send_static_file('index.html')
 
+# Route Wind Speed Request to Non-Cleansed Linear Model
+@app.route('/api/linearmodelnc/<string:windSpeed>')
+def linearmodelnc(windSpeed):
+    
+    # String must be converted to float as cannot pass natively
+    # Insight from https://github.com/pallets/flask/issues/315
+    windSpeed = float(windSpeed)
+    return lm.nc_linear_input(windSpeed)
 
-@app.route('/api/linearmodelnc')
-def index():
-    return lm.nc_linear_input(20)
 
+# Route Wind Speed Request to Non-Cleansed Linear Model
+@app.route('/api/linearmodelc/<string:windSpeed>')
+def linearmodelc(windSpeed):
+    
+    # String must be converted to float as cannot pass natively
+    # Insight from https://github.com/pallets/flask/issues/315
+    windSpeed = float(windSpeed)
+    return lm.c_linear_input(windSpeed)
 
 
 if __name__ == "__main__":
