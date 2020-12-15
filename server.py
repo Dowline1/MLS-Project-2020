c9847c120c38026139811286f0850127e3158d63
Dowling, Eoin (00431819) <Eoin.Dowling@bms.com>
2020-12-15 22:48:08 +0000

    Commenced Work on Docker File and Bringing Neural Network Model into Server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import flask as fl
 from linearModel import LinearModel as lm
+from sigmoid_nn import SigmoidModel as sm
 
 # Create Web App
 app = fl.Flask(__name__, static_url_path = '', static_folder = 'staticpages')
@@ -26,6 +27,23 @@ def linearmodelc(windSpeed):
     # Insight from https://github.com/pallets/flask/issues/315
     windSpeed = float(windSpeed)
     return lm.c_linear_input(windSpeed)
+
+
+# Trains Sigmoid Model
+#@app.route('/api/trainSigmoid')
+#def trainSModel():
+
+#    return sm.trainSigmoidModel()
+
+
+# Route Wind Speed Request to Non-Cleansed Linear Model
+@app.route('/api/trainSigmoid/<string:windSpeed>')
+def sigmoidModelPredict(windSpeed):
+    
+    # String must be converted to float as cannot pass natively
+    # Insight from https://github.com/pallets/flask/issues/315
+    windSpeed = float(windSpeed)
+    return sm.trainSigmoidModel(windSpeed)
 
 
 if __name__ == "__main__":
---
+++ sigmoid_nn.py
@@ -0,0 +1,48 @@
+# Neural networks
+import tensorflow.keras as kr
+import pandas as pd
+import numpy as np
+
+# Read Dataset into Dataframe
+power_data = pd.read_csv("Resources/power_dataset.csv")
+
+# Speed = 0 & Power = 0
+clean_data = power_data[(power_data["speed"]==0) & (power_data["power"]==0)]
+
+# Speed > 0 & Power > 0
+non_zero = power_data[(power_data["speed"]>0) & (power_data["power"]>0)]
+
+# Combine Data
+clean_data = clean_data.append(non_zero)
+
+# Sigmoid Neural Network Model Class
+#-------------------------------------------------------------------------------#
+class SigmoidModel:
+    def trainSigmoidModel(x):         
+    
+        # Train Model using Sigmoid Activation which most closely matches data
+        model = kr.models.Sequential()
+        
+        # Use 50 Sigmoid Neuron Layer
+        model.add(kr.layers.Dense(50, input_shape=(1,), activation='sigmoid', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+        
+        # Use Single Linear Layer to ensure numbers align with raw data else will stay < 1
+        model.add(kr.layers.Dense(1, activation='linear', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+        model.compile(kr.optimizers.Adam(lr=0.001), loss='mean_squared_error')
+
+        # Fit the data/Train Model
+        # Trys to get lowest cost score by small changes
+        model.fit(clean_data['speed'], clean_data['power'], epochs=500, batch_size=10)
+   
+        # Make Prediction Using Wind Speed 20
+        prediction = model.predict([x])
+
+        # Stores elemtent of array as variable
+        result = np.ndarray.item(prediction[0])
+
+        return {"value": result}
+
+result = SigmoidModel.trainSigmoidModel(20)
+
+print(result)
+print(type(result))
